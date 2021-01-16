11ecfc6248ddfbde611384f3bc40a8bc58597727
Gucyeter <gucyeterg@metr.mee.com>
2021-01-16 23:43:32 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ckamp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
